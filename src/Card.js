4ba847e6718b5695cf7d195aa64f70393ac3124a
Davron <davronmamaraimov1996@gmail.com>
2022-09-28 22:31:50 +0500

    Styles were added to App.css

--- src/Card.js
+++ src/Card.js
@@ -10,8 +10,7 @@ const Card = () => {
                     <img src="https://irs.www.warnerbros.com/keyart-jpeg/movies/media/browser/the_great_gatsby_key_art.jpg" alt="" />
                     <h1>Author: F. Scott Fitzgerald</h1>
                     <h2>Published: April 10, 1925</h2>
-                    <p>Definition: Lorem ipsum dolor, 
-                    sit amet consectetur adipisicing elit. Ex magnam possimus sit, esse, debitis ea vel, laborum rem porro quis tempore cumque totam nihil iste natus necessitatibus hic eveniet. Officia!</p>
+                    <p><b>Definition:</b> Ex magnam possimus sit, esse, essitatibus hic eveniet. Officia!</p>
                     <h3>Price: $ 35</h3>
                 </div>
             </div>
